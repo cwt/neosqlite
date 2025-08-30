e604e4b117dc81248c734dfcd265b19c0ff88b61
Chaiwat Suttipongsakul <cwt@bashell.com>
2025-08-31 02:04:25 +0700

    Run pytest in parallel

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+CPU_CORES=$(lscpu | awk '/^Core\(s\) per socket:/ {c=$4} /^Socket\(s\):/ {s=$2} END {print c * s}')
+
 rm -rf */__pycache__ .pytest_cache
 poetry update
 poetry install
-poetry run pytest tests/ --cov=neosqlite --cov-report=term-missing --cov-fail-under=85
+poetry run pytest tests/ -n $CPU_CORES --cov=neosqlite --cov-report=term-missing --cov-fail-under=85
